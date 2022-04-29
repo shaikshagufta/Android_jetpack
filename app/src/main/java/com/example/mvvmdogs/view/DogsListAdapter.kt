5c5f1a3498f635e8f7197c4edceb23ecf19d233b
sshagufta3 <shaikshagufta99@gmail.com>
2022-04-29 11:59:53 +0530

    DataBinding-comment cleanUp

--- app/src/main/java/com/example/mvvmdogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/example/mvvmdogs/view/DogsListAdapter.kt
@@ -11,8 +11,6 @@ import com.example.mvvmdogs.model.DogBreed
 
 class DogsListAdapter(private val dogList: ArrayList<DogBreed>) : RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(){
 
-    //private lateinit var binding: ItemDogBinding
-
     //to update the list when we get info from backend
     fun updateDogList(newDogList: List<DogBreed>) {
         dogList.clear()
@@ -22,10 +20,8 @@ class DogsListAdapter(private val dogList: ArrayList<DogBreed>) : RecyclerView.A
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        // val view = inflater.inflate(R.layout.item_dog, parent, false)
-        val view = DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
-
-        //binding = ItemDogBinding.inflate(inflater, parent, false)
+        val view =
+            DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
 
         //returning the ViewHolder containing the inflated view as parameter
         return DogViewHolder(view)
@@ -36,28 +32,11 @@ class DogsListAdapter(private val dogList: ArrayList<DogBreed>) : RecyclerView.A
 
         //attaching the layout variable we created for databinding
         holder.view.dog = dogList[position]
-        //with data binding we don't need to specify in text which elements we want to update, like we did before
-       /* holder.name.text= dogList[position].dogBreed
-        holder.lifeSpan.text = dogList[position].lifeSpan
-        //nav to detail fragment on click
-        holder.view.setOnClickListener {
-            val action = ListFragmentDirections.actionDetailFragment()
-            action.dogUuid = dogList[position].uuid//so that we get a uuid in 'action'
-            Navigation.findNavController(it).navigate(action)
-        }
-        holder.imageView.loadImage(dogList[position].imageUrl, getProgressDrawable(holder.imageView.context))*/
+
     }
 
     override fun getItemCount() = dogList.size
 
     //inner class we created before inheritance
-    //1. add the generated binding layout instead of view and pass the root as the view
-    inner class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root){
-
-        //we avoid all this using data binding
-
-        //val name: TextView = view.findViewById(R.id.name)
-        //val lifeSpan: TextView = view.findViewById(R.id.lifeSpan)
-        //val imageView: ImageView = view.findViewById(R.id.imageView)
-    }
+    inner class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
 }
