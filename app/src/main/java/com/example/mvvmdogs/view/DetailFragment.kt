4a7b34953de454e70b2481f61e0a31c6ee360baa
sshagufta3 <shaikshagufta99@gmail.com>
2022-05-02 14:29:08 +0530

    Palette

--- app/src/main/java/com/example/mvvmdogs/view/DetailFragment.kt
+++ app/src/main/java/com/example/mvvmdogs/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mvvmdogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +9,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.mvvmdogs.databinding.FragmentDetailBinding
+import com.example.mvvmdogs.model.DogPalette
 import com.example.mvvmdogs.viewmodel.DetailViewModel
 
 class DetailFragment : Fragment() {
@@ -47,8 +54,33 @@ class DetailFragment : Fragment() {
         viewModel.dogLiveData.observe(viewLifecycleOwner, Observer { dog ->
             dog?.let {
                 binding.dog = dog
+
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
     }
 
+    private fun setupBackgroundColor(url: String) {
+        // we use glide to load an image and use it to add some functionality using it
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>(){
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                  Palette.from(resource)
+                      .generate { palette ->
+                        val intColor = palette?.mutedSwatch?.rgb ?: 0//extract the preferred color
+                        val myPalette =  DogPalette(intColor)
+                          binding.palette = myPalette
+
+                      }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {                }
+
+            })//custom object used to access the image
+    }
 }
